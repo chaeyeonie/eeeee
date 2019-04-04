2ed01dd5802eb6ae2e6b0347f1ead8140da51c93
lee chaeyeon <chaeyeoni345@gmail.com>
2019-04-04 16:21:29 +0900

    11

---
+++ mod.py
@@ -0,0 +1,38 @@
+class Fourcal:
+      def __init__(self, first, second):
+          self.first = first
+          self.second = second
+    def setdata(self, first, second):
+        self.first = first
+         self.second = second
+def add(self):
+    result = self.first + self.second
+    return result
+def mul(self):
+    result = self.first * self.second
+    return result
+def sub(self):
+    result = self. first - self.second
+    return result
+def div(self):
+    result = self.fisrt / self.second
+    return result
+class AbcFourCal(Fourcal):
+    def div(self):
+        if self.second == 0 :
+            return "`o`"
+        else:
+            return self.first / self.second
+class BceFourCal(Fourcal):
+    def num(self):
+        if self.first and self.second >5:
+            return "`0`"
+class CdeFourCal(FourCal):
+    def num(self):
+        if self.first == 5 and self.second = "stop":
+            return
+        else: 
+            continue
+       
+
+
